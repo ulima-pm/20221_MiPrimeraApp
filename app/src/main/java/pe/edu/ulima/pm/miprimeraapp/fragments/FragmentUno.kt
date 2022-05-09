c552b5cea7c266cde4dd3ad2720fe5e251f9821c
Hernan <hernan@jsatch.com>
2022-05-09 12:17:43 -0500

    Trabajando con fragments

---
+++ app/src/main/java/pe/edu/ulima/pm/miprimeraapp/fragments/FragmentUno.kt
@@ -0,0 +1,32 @@
+package pe.edu.ulima.pm.miprimeraapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import pe.edu.ulima.pm.miprimeraapp.R
+
+class FragmentUno : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_uno,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.butFragmentUno).setOnClickListener {
+            Toast.makeText(activity, "Fragment UNO", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
