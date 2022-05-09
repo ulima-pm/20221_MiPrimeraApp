1569f5f42b3c9cf9285ce9adb5dc265b4599d031
Hernan <hernan@jsatch.com>
2022-05-09 13:18:19 -0500

    Utilizando fragment transactions

--- app/src/main/java/pe/edu/ulima/pm/miprimeraapp/MenuActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/miprimeraapp/MenuActivity.kt
@@ -2,10 +2,55 @@ package pe.edu.ulima.pm.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import pe.edu.ulima.pm.miprimeraapp.fragments.FragmentDos
+import pe.edu.ulima.pm.miprimeraapp.fragments.FragmentUno
 
 class MenuActivity : AppCompatActivity() {
+    val fragments : List<Fragment> = listOf(
+        FragmentUno(), FragmentDos()
+    )
+
+    var mCurrentFragmentIndex : Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val butIrFragmentUno : Button = findViewById(R.id.butIrFragmentUno)
+        val butIrFragmentDos : Button = findViewById(R.id.butIrFragmentDos)
+
+        butIrFragmentUno.setOnClickListener {
+            if (mCurrentFragmentIndex != 0)
+            {
+                mCurrentFragmentIndex = 0
+                val ft : FragmentTransaction = supportFragmentManager.beginTransaction()
+                ft.replace(R.id.fcvFragments, fragments[mCurrentFragmentIndex])
+                ft.commit()
+
+            }
+
+        }
+
+        butIrFragmentDos.setOnClickListener {
+            if (mCurrentFragmentIndex != 1)
+            {
+                mCurrentFragmentIndex = 1
+                val ft : FragmentTransaction = supportFragmentManager.beginTransaction()
+                ft.replace(R.id.fcvFragments, fragments[mCurrentFragmentIndex])
+                ft.commit()
+
+            }
+
+        }
+
+        // Agregamos fragment por defecto
+        val ft : FragmentTransaction = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fcvFragments, fragments[mCurrentFragmentIndex])
+        ft.commit()
     }
 }
